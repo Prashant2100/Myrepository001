313d72fde28c560579d48561b931fd3eb1ba8328
Prashant2100 <98760335+Prashant2100@users.noreply.github.com>
2022-02-10 21:34:17 +0530

    Add files via upload
    add python file for first time!

---
+++ mlprac.py
@@ -0,0 +1,18 @@
+a=int(input())
+for mm in range(a):
+    l=list(map(int,input().split()))
+    a=l[0]
+    b=l[1]
+    print(a,b)
+    k=[]
+    for i in range(a+1):
+        for t in range(a+1):
+            j=[]
+            if(i+t)==b:
+                
+                j.append(i)
+                j.append(t)
+                k.append(j)
+    k.sort()
+    print(k[0][1]-k[0][0])
+        
